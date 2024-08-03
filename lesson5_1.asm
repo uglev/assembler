676ddc036bed5dfa97414bdfa339ea403626b2e3
uglev <46020541+uglev@users.noreply.github.com>
2024-08-03 20:46:15 +0300

    Add files via upload

---
+++ lesson5_1.asm
@@ -0,0 +1,23 @@
+﻿.286
+CSEG segment
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG
+org 100h 
+begin:
+;Всё написанное выше пока опускаем.
+mov cx,5					;Устанавливаем значение счётчика в 5.
+
+metka:
+mov ah,9					;Загружаем в регистр ah число 9 (указываем функцию).
+mov dx,offset helloworld			;Указываем, что за фразу мы будем выводить.
+int 21h 					;Выводим фразу.
+loop metka					;Переходим на метку metka и уменьшаем cx на 1.
+
+int 20h 					;Выходим в DOS.
+
+helloworld db 'Hello, world!$'			;Определяем переменную helloworld, доступную побайтно, с фразой
+						;"Hello, world!". В одинарных кавычках, после знака "!" ставим
+						;знак "$".
+
+;Завершение программы.
+CSEG ends 					
+end begin         
---
+++ lesson_game.asm
@@ -0,0 +1,63 @@
+﻿.286
+CSEG segment   
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG       
+org 100h 					
+begin:
+; ds указывает на видеопамять
+push 0b800H
+pop ds
+; установить графический режим 40x25
+int 10H
+; bx = 700H - смещение, по которому находиться грузовик
+mov bh, 7H
+
+main_loop:
+; Задержка и вывод грузовика на экран
+xchg cx, ax ; mov ah, 0
+int 1AH
+mov [bx], dl
+delay:
+int 1AH
+cmp [bx], dl
+je delay
+
+; si - смещение следующего препятствия
+xchg ax, si
+add al, dl
+xchg ax, si
+
+xchg ax, cx ; mov cx, 0
+
+; Получение нажатой клавиши
+in al, 60H
+cmp al, 77
+jnz keytest1 
+; вправо
+inc bx
+inc bx 
+keytest1:
+; влево
+ja keytest2
+dec bx
+dec bx
+keytest2:
+; очистка буфера клавиатуры
+mov ah, 0CH
+int 21H
+; скролл экрана на 1 строчку
+mov ax, 0701H
+mov dx, 1827H
+int 10H
+
+; вывод препятствия
+mov [si], ax
+; вывод травы и разделительной полосы
+mov [di+51], dx
+; проверка что перед грузовиком нет препятствий
+cmp [bx], dh
+ja main_loop
+
+ret
+
+CSEG ends
+end begin
