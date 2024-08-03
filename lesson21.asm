63014c6f0e0a37c557c9a8d23a0cb093b0a770e1
uglev <46020541+uglev@users.noreply.github.com>
2024-08-03 20:52:53 +0300

    Add files via upload

---
+++ lesson21.asm
@@ -0,0 +1,48 @@
+﻿.286
+CSEG segment
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG
+org 100h
+begin:
+                                             	 ;Стандартное начало com-файла.
+
+mov ax,3D02h
+mov dx,offset File_name
+int 21h
+mov Handle,ax
+mov bx,ax                                        ;Открыли файл, сохранили bx.
+
+mov ah,3Fh
+mov dx,offset Finish
+mov cx,9
+int 21h 
+                                           
+mov Length_F,cx                                  ;Прочитали файл, в cx - его истинная длина.
+mov si,dx                                        ;Перенесём dx в si для работы с циклом.
+
+cycle:                                     	 ;Основной цикл. В si - адрес начала загруженного файла.
+mov ah,byte ptr cs:[si]                     	 ;Прочтём первый байт в ah
+inc ah                                     	 ;Считанное значение в ah увеличим на 1.         
+mov byte ptr cs:[si],ah                     	 ;Запишем обратно.                               
+inc si                                      	 ;Перейдём к следующему байту.                   
+loop cycle                                  	 ;В cx - длина. Пока не обнулится cx - на метку.
+
+mov ax,4200h
+xor dx,dx
+xor cx,cx
+int 21h
+mov ah,40h
+mov dx,offset Finish
+mov cx,Length_F
+int 21h
+mov ah,3Eh                                                             
+mov bx,Handle
+int 21h                                          ;Сохранили файл с начала и закрыли его.
+
+ret                                              ;Можно выйти в DOS и так.
+
+File_name db '111.txt',0,'!$'                    ;Текстовые переменные.
+Handle dw 0
+Length_F dw 0
+Finish equ $
+CSEG ends
+end begin
---
+++ lesson23.asm
@@ -0,0 +1,38 @@
+﻿CSEG segment
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG
+org 100h
+
+begin:
+mov ax,03h
+int 10h         			;графический режим 80х25.
+mov bl,03h          			;цвет линии.
+mov cx,1
+mov dx,0      				;начальные координаты точки.
+ 
+mainloop:
+mov ah,7        			;ждём символ с клавиатуры.
+int  21h
+ 
+cmp al,27       			;...это ESC?
+je quit        				;да - на выход!
+  
+cmp al,4dh       			;сравнение со стрелкой "вправо".
+je key_right     			;переход в обработчик.
+jmp mainloop    			;если да, не выходим, а ждём другую клавишу.
+ 
+draw:                                   ;рисование линии.
+mov ah,09h
+mov al,0C4h        			;горизонтальная линия (0B3h - вертикальная).
+mov bh,00          			;страница 0 
+int 10h
+jmp mainloop     			;продолжаем ждать символ.
+ 
+key_right:           			;обработчик клавиши "вправо".
+inc cx           			;рисуем ещё точку
+jmp draw         
+ 
+quit:               			;обработчик клавиши ESC.
+int 20h
+
+CSEG ends
+end begin
